79d6369d3025b9598273f624575d0c842e0ca5ed
Benedikt Óskarsson <benedikto16@ru.is>
2017-10-23 18:23:52 +0000

    Added welcomeMessage

--- src/main/java/is/ru/ticTacToe/ConsoleUI.java
+++ src/main/java/is/ru/ticTacToe/ConsoleUI.java
@@ -2,4 +2,12 @@ package is.ru.ticTacToe;
 
 public class ConsoleUI {
 
+    ConsoleUI (){
+        GameLogic runningGame = new GameLogic();
+        welcomeMessage();
+    }
+
+    void welcomeMessage(){
+        System.out.println("Welcome to Tic Tac Toe");
+    }
 }
