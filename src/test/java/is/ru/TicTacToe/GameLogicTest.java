888f3308576d1dab3e8048d644e7c533a98e71de
Benedikt Óskarsson <bensi94@hotmail.com>
2017-10-23 17:03:30 +0000

    GameLogicTest setup (#3)
    * GameLogicTest setup
    
    * Fixed before setup

---
+++ src/test/java/is/ru/TicTacToe/GameLogicTest.java
@@ -0,0 +1,13 @@
+package is.ru.ticTacToe;
+
+import org.junit.Before;
+
+public class GameLogicTest {
+
+    private GameLogic gameLogicTest;
+
+    @Before
+    public void setUp() throws Exception {
+        gameLogicTest = new GameLogic();
+    }
+}
