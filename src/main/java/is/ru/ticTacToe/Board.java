04f9df3239512f8a17d01b68241d943d6cf60c67
orria16 <orria16@ru.is>
2017-10-23 18:51:04 +0000

    add two function in board class

--- src/main/java/is/ru/ticTacToe/Board.java
+++ src/main/java/is/ru/ticTacToe/Board.java
@@ -2,4 +2,30 @@ package is.ru.ticTacToe;
 
 public class Board {
 
+	private char arrayBoard[][];
+
+	public Board() {
+		arrayBoard = new char[3][3];
+	}
+
+	public void defaultBoard() {
+		char counter = '1';
+		for (int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+		 		arrayBoard[i][j] = counter;
+		 		counter++;
+			}
+		}
+	}
+
+	public void markBoard(int index, char input) {
+		for (int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+		 		if ((int) (arrayBoard[i][j]) == index) {
+		 			arrayBoard[i][j] = input;
+		 			break;
+		 		}
+			}
+		}
+	}
 }
