7bb2946bc9d27e6d618cffac13e7227cba379b67
Benedikt Óskarsson <benedikto16@ru.is>
2017-10-23 18:31:45 +0000

    Game logic constructor

--- src/main/java/is/ru/ticTacToe/GameLogic.java
+++ src/main/java/is/ru/ticTacToe/GameLogic.java
@@ -1,5 +1,12 @@
 package is.ru.ticTacToe;
 
 public class GameLogic {
+    private Board board;
+    private Player playerOne, playerTwo;
 
+    GameLogic(){
+        board = new Board();
+        playerOne = new Player();
+        playerTwo = new Player();
+    }
 }
