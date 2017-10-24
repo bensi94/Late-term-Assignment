d6776238124dc006919a69a979d02c8489df79c4
SteinarTh <steinart16@ru.is>
2017-10-24 17:14:49 +0000

    winner prototype

--- src/main/java/is/ru/ticTacToe/GameLogic.java
+++ src/main/java/is/ru/ticTacToe/GameLogic.java
@@ -19,5 +19,33 @@ public class GameLogic {
         return gameRunning;
     }
 
+    public boolean winner(){
+  /*  0,0 0,1 0,2
+      1,0 1,1 1,2
+      2,0 2,1 2,2
+    */  // Horizontal
+        for(int i = 0; i < 3; i++){
+          if(NAMEOFARRAY[i][0] && NAMEOFARRAY[i][1] && NAMEOFARRAY[i][2] == ('X' | 'O')){
+            // TODO:::
+          }
+        }
+
+        // Vertial
+        for(int j = 0; j < 3; j++) {
+          if(NAMEOFARRAY[0][j] && NAMEOFARRAY[1][j] && NAMEOFARRAY[2][j] == ('X' | 'O')){
+            // TODO:::
+          }
+        }
+
+        // Diagonally left
+          if (NAMEOFARRAY[0][0] && NAMEOFARRAY[1][1] && NAMEOFARRAY[2][2] == ('X' | 'O')){
+            // TODO:::
+        }
+        if (NAMEOFARRAY[0][2] && NAMEOFARRAY[1][1] && NAMEOFARRAY[2][0] == ('X' | 'O')){
+          // TODO:::
+      }
+
+    }
+
 
 }
