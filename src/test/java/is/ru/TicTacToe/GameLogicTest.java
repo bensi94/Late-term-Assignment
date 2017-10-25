7245fe964d444994bf7c88e5672bfad079643ff9
SteinarTh <steinart16@ru.is>
2017-10-25 17:42:09 +0000

    Added vertical and diogonal tests for winner

--- src/test/java/is/ru/TicTacToe/GameLogicTest.java
+++ src/test/java/is/ru/TicTacToe/GameLogicTest.java
@@ -54,4 +54,24 @@ public class GameLogicTest {
       gameLogicTest.markBoard(4);
       assertEquals(-1,gameLogicTest.winner());
     }
+
+    @Test
+    public void testWinnerVertical(){
+      gameLogicTest.markBoard(2);
+      gameLogicTest.markBoard(4);
+      gameLogicTest.markBoard(5);
+      gameLogicTest.markBoard(3);
+      gameLogicTest.markBoard(8);
+      assertEquals(1,gameLogicTest.winner());
+    }
+
+    @Test
+    public void testWinnerDigonally(){
+      gameLogicTest.markBoard(1);
+      gameLogicTest.markBoard(4);
+      gameLogicTest.markBoard(5);
+      gameLogicTest.markBoard(3);
+      gameLogicTest.markBoard(9);
+      assertEquals(1,gameLogicTest.winner());
+    }
 }
