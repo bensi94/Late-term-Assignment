5a2165d1ebf442a1cba185899ce37cc2742dc12d
Benedikt Óskarsson <benedikto16@ru.is>
2017-10-25 19:37:40 +0000

    Trying to get react into the project, code not working atm

--- src/main/java/is/ru/ticTacToe/TicTacToeMain.java
+++ src/main/java/is/ru/ticTacToe/TicTacToeMain.java
@@ -1,14 +1,23 @@
 package is.ru.ticTacToe;
 
 import static spark.Spark.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.core.env.StandardEnvironment;
 
+
+@SpringBootApplication
 public class TicTacToeMain {
     public static void main(String[] args){
-        //ConsoleUI console = new ConsoleUI();
-        port(getHerokuPort());
-        get("/", (reg, res) -> {
-            return "<h1>Steini er maðurinn</h1>";
-        });
+        //ConsoleUI console = new ConsoleUI();;
+        //Logger logger = LoggerFactory.getLogger(TicTacToeMain.class);
+        SpringApplication.run(TicTacToeMain.class, args);
+        // port(getHerokuPort());
+        // get("/", (reg, res) -> {
+        //     return "<h1>Steini er maðurinn</h1>";
+        // });
     }
 
     static int getHerokuPort(){
