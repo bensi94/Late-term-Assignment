8f76163a11b00b919adbebf3982b1a6b42ac1769
Benedikt Óskarsson <benedikto16@ru.is>
2017-10-25 12:47:32 +0000

    Procfile added, spark added build script changed

--- src/main/java/is/ru/ticTacToe/TicTacToeMain.java
+++ src/main/java/is/ru/ticTacToe/TicTacToeMain.java
@@ -1,7 +1,22 @@
 package is.ru.ticTacToe;
 
+import static spark.Spark.*;
+
 public class TicTacToeMain {
     public static void main(String[] args){
-        ConsoleUI console = new ConsoleUI();
+        //ConsoleUI console = new ConsoleUI();
+        port(getHerokuPort());
+        get("/", (reg, res) -> {
+            return "Hello TicTacToe";
+        });
+    }
+
+    static int getHerokuPort(){
+        ProcessBuilder psb = new ProcessBuilder();
+        if(psb.environment().get("PORT") != null){
+            return Integer.parseInt(psb.environment().get("PORT"));
+        }
+
+        return 4567;
     }
 }
