4f3a74f2e477e5396562174d8edaad4b1cdff881
Benedikt Óskarsson <benedikto16@ru.is>
2017-10-26 15:23:46 +0000

    Changeing to backend

---
+++ backend/src/main/java/is/ru/ticTacToe/TicTacToeMain.java
@@ -0,0 +1,27 @@
+package is.ru.ticTacToe;
+
+import com.github.springfox.loader.EnableSpringfox;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;;
+
+@EnableSpringfox
+@SpringBootApplication
+public class TicTacToeMain {
+    public static void main(String[] args){
+        //ConsoleUI console = new ConsoleUI();
+        // port(getHerokuPort());
+        // get("/", (reg, res) -> {
+        //     return "<h1>Steini er maðurinn</h1>";
+        // });
+        SpringApplication.run(TicTacToeMain.class, args);
+    }
+
+    // static int getHerokuPort(){
+    //     ProcessBuilder psb = new ProcessBuilder();
+    //     if(psb.environment().get("PORT") != null){
+    //         return Integer.parseInt(psb.environment().get("PORT"));
+    //     }
+    //
+    //     return 4567;
+    //}
+}
