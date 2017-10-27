6da4b23ddad43982c6f89251b81804ee98ad2453
Benedikt Óskarsson <benedikto16@ru.is>
2017-10-27 12:31:55 +0000

    Able to run application with java and react togeather

---
+++ backend/src/main/java/is/ru/ticTacToe/FrontendController.java
@@ -0,0 +1,9 @@
+package is.ru.ticTacToe;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FrontendController {
+
+}
