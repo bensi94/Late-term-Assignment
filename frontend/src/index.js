5d89f0fbca8282fb7384d12c29df5c1c74f7eff5
Benedikt Óskarsson <benedikto16@ru.is>
2017-10-27 16:06:58 +0000

    Port for js app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,3 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
