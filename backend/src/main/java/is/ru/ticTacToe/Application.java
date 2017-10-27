00f10bd5dfcf4b0991587f937c988337c033b806
Benedikt Óskarsson <benedikto16@ru.is>
2017-10-27 20:14:36 +0000

    Clean up

--- backend/src/main/java/is/ru/ticTacToe/Application.java
+++ backend/src/main/java/is/ru/ticTacToe/Application.java
@@ -8,20 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
-        //ConsoleUI console = new ConsoleUI();
-        // port(getHerokuPort());
-        // get("/", (reg, res) -> {
-        //     return "<h1>Steini er maðurinn</h1>";
-        // });
         SpringApplication.run(Application.class, args);
     }
-
-    // static int getHerokuPort(){
-    //     ProcessBuilder psb = new ProcessBuilder();
-    //     if(psb.environment().get("PORT") != null){
-    //         return Integer.parseInt(psb.environment().get("PORT"));
-    //     }
-    //
-    //     return 4567;
-    //}
 }
