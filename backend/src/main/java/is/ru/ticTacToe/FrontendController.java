70d0edde08f208f4217f9ab8c9bac9c3e10c445e
Benedikt Óskarsson <benedikto16@ru.is>
2017-10-28 01:15:00 +0000

    Java showing board on '/board'

--- backend/src/main/java/is/ru/ticTacToe/FrontendController.java
+++ backend/src/main/java/is/ru/ticTacToe/FrontendController.java
@@ -1,14 +1,30 @@
 package is.ru.ticTacToe;
 
+import com.google.common.collect.ImmutableMap;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class FrontendController {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "lalalal";
+    private GameLogic logicForController = new GameLogic();
+
+    @RequestMapping(value = "/board", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String board() {
+        String jsonBoard = "";
+        char [][] charBoard = logicForController.getBoard();
+        char counter = '1';
+        for (int i = 0; i < charBoard.length; i++ ) {
+            for (int j = 0; j < charBoard[i].length; j++){
+                jsonBoard += "{\"" + counter + "\":\""+ charBoard[i][j] +"\"}";
+                counter++;
+            }
+        }
+        jsonBoard = "{\"board\":\"" + jsonBoard + "\"}";
+        return jsonBoard;
     }
 }
