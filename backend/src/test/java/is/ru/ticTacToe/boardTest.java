319d3089717b47b0a91aff7625b1ce369268dfd5
Daníel Melstað <danielm15@ru.is>
2017-10-29 20:55:24 +0000

    Added Unit Tests

--- backend/src/test/java/is/ru/ticTacToe/boardTest.java
+++ backend/src/test/java/is/ru/ticTacToe/boardTest.java
@@ -1,16 +1,42 @@
 package is.ru.ticTacToe;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 
 public class boardTest {
 
-	Board board = new Board(3);
+	private Board boardTest;
+	
+	@Before
+    public void setUp() throws Exception {
+        boardTest = new Board(3);
+    }
+
 	@Test
 	public void TestValue() {
-		board.markBoard(9, 'X');
-		assertEquals('X', board.getBoard()[2][2]);
+		boardTest.markBoard(9, 'X');
+		assertEquals('X', boardTest.getBoard()[2][2]);
 	}
 
+	@Test(expected=IllegalArgumentException.class)
+	public void testException() {
+		boardTest = new Board(0);
+	}
+
+	@Test
+	public void TestInvalidIndex() {
+		assertEquals(false, boardTest.markBoard(0, 'X'));
+		boardTest.markBoard(1, 'X');
+		assertEquals(false, boardTest.markBoard(1, 'X'));
+		assertEquals(false, boardTest.markBoard(10, 'X'));
+	}
+
+	@Test
+	public void TestSize() {
+		assertEquals(3, boardTest.size());
+	}
 }
